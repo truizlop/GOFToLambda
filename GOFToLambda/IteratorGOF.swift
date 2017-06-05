1edaf57f54ab78e76a8b493237e781274d35537f
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-06-05 12:11:00 +0200

    Implement Iterator GOF Style

---
+++ GOFToLambda/IteratorGOF.swift
@@ -0,0 +1,17 @@
+//
+//  IteratorGOF.swift
+//  GOFToLambda
+//
+//  Created by Tomás Ruiz López on 5/6/17.
+//  Copyright © 2017 Tomás Ruiz-López. All rights reserved.
+//
+
+import Foundation
+
+func runIteratorGOF() {
+    let list = [1, 2, 3, 4, 5, 6]
+    
+    for item in list {
+        print(item)
+    }
+}
