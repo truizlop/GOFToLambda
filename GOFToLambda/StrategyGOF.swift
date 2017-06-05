59b921ea05b5693df16f621a8c61bbd3049eb2d2
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-06-05 13:06:52 +0200

    Implement Strategy GOF Style

---
+++ GOFToLambda/StrategyGOF.swift
@@ -0,0 +1,60 @@
+//
+//  StrategyGOF.swift
+//  GOFToLambda
+//
+//  Created by Tomás Ruiz López on 5/6/17.
+//  Copyright © 2017 Tomás Ruiz-López. All rights reserved.
+//
+
+import Foundation
+
+protocol TextFormatter {
+    func filter(text : String) -> Bool
+    func format(text : String) -> String
+}
+
+struct PlainTextFormatter : TextFormatter {
+    func filter(text: String) -> Bool {
+        return true
+    }
+    
+    func format(text: String) -> String {
+        return text
+    }
+}
+
+struct ErrorTextFormatter : TextFormatter {
+    func filter(text: String) -> Bool {
+        return text.hasPrefix("ERROR")
+    }
+    
+    func format(text: String) -> String {
+        return text.uppercased()
+    }
+}
+
+struct ShortTextFormatter : TextFormatter {
+    func filter(text: String) -> Bool {
+        return text.characters.count < 20
+    }
+    
+    func format(text: String) -> String {
+        return text.lowercased()
+    }
+}
+
+struct TextEditor {
+    let formatter : TextFormatter
+    
+    func publish(text : String) {
+        if formatter.filter(text: text) {
+            print(formatter.format(text: text))
+        }
+    }
+}
+
+func runStrategyGOF() {
+    let editor = TextEditor(formatter: ErrorTextFormatter())
+    editor.publish(text: "ERROR - something bad happened")
+    editor.publish(text: "DEBUG - I'm here")
+}
