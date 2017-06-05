15f35e1ee515f43eade1031df6d2878dcfe2fcdc
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-06-05 10:34:53 +0200

    Initial Commit

---
+++ GOFToLambda/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GOFToLambda
+//
+//  Created by Tomás Ruiz López on 5/6/17.
+//  Copyright © 2017 Tomás Ruiz-López. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
