977d0a00fe6767c222803df4691775058c257b88
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-06-05 12:12:16 +0200

    Implement Iterator Lambda Style

---
+++ GOFToLambda/IteratorLambda.swift
@@ -0,0 +1,13 @@
+//
+//  IteratorLambda.swift
+//  GOFToLambda
+//
+//  Created by Tomás Ruiz López on 5/6/17.
+//  Copyright © 2017 Tomás Ruiz-López. All rights reserved.
+//
+
+import Foundation
+
+func runIteratorLambda() {
+    [1, 2, 3, 4, 5, 6].forEach{ print($0) }
+}
